af521b6a9c773734673f34724eac1bccf6bb1664
Privatik3 <lol11999333@gmail.com>
2018-05-21 10:59:17 +0300

    Валидация имени телефона в форме оформления

--- catalog/controller/checkout/confirm.php
+++ catalog/controller/checkout/confirm.php
@@ -7,7 +7,7 @@ class ControllerCheckoutConfirm extends Controller
 
         $redirect = '';
         // Validate cart has products and has stock.
-        if ((!$this->cart->hasProducts() && empty($this->session->data['vouchers'])) || (!$this->cart->hasStock() && !$this->config->get('config_stock_checkout'))) {
+        if (!$this->cart->hasProducts()) {
             $redirect = $this->url->link('checkout/cart');
         }
 
